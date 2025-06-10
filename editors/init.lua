33042da7ce7390e83e2c2057a1e72f07a19932fb
Yury Nikiforov <brand_new_replicant@protonmail.com>
2025-06-10 13:25:59 +0300

    update init.lua

--- editors/init.lua
+++ editors/init.lua
@@ -1,67 +1,90 @@
---[[
-General settings
---]]
-vim.o.showcmd = true
-vim.o.showmatch = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.incsearch = true
-vim.o.autowrite = true
-vim.o.hidden = true
-vim.o.mouse = 'a'
-vim.o.hlsearch = true
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.bo.smartindent = true
+vim.opt.showcmd = true
+vim.opt.showmatch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.autowrite = true
+vim.opt.hidden = true
+vim.opt.hlsearch = false
+vim.opt.termguicolors = false
+vim.opt.wrap = true
+vim.opt.pumheight = 10
+vim.opt.showtabline = 2
+vim.opt.laststatus = 2
+vim.opt.showmode = true
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
+vim.opt.completeopt = "menu,menuone,noselect,popup"
+vim.opt.syntax = on
+vim.opt.omnifunc = "v:lua.vim.lsp.omnifunc"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorcolumn = true
+vim.opt.cursorline = true
+vim.opt.list = false
+vim.opt.colorcolumn = "80"
+vim.o.listchars = "space:·,eol:$,tab:>-,trail:·,extends:>,precedes:<"
+
+vim.g.netrw_liststyle = 3
+vim.g.netrw_banner = 0
+vim.g.netrw_browse_split = 0
+
+vim.g.editorconfig = false
+
+vim.bo.smartindent = false
 vim.bo.cindent = true
 vim.bo.expandtab = true
 vim.bo.tabstop = 2
 vim.bo.shiftwidth = 2
-vim.wo.cursorcolumn = true
-vim.wo.cursorline = true
-vim.bo.omnifunc = 'syntaxcomplete#Complete'
 vim.bo.autoindent = true
-vim.wo.list = true
---[[
-IDE-like symbols
---]]
-vim.o.listchars = 'space:·,eol:$,tab:>-,trail:·,extends:>,precedes:<'
---[[
-bash language server
---]]
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'sh',
-  callback = function()
-    vim.lsp.start({
-      name = 'bash-language-server',
-      cmd = { 'bash-language-server', 'start' },
-    })
-  end,
-})
---[[
-Python (Ruff) language server
---]]
-require('lspconfig').ruff_lsp.setup {
-  init_options = {
-    settings = {
-      -- Any extra CLI arguments for `ruff` go here.
-      args = {},
-    }
-  }
-}
---[[
-Go language server - not tested yet
-local lspconfig = require("lspconfig")
-lspconfig.gopls.setup({
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparams = true,
-      },
-      staticcheck = true,
-      gofumpt = true,
-    },
-  },
-})
---]]
 
+vim.api.nvim_set_keymap("i", "(", "()<Left>", { noremap = true })
+vim.api.nvim_set_keymap("i", "{", "{}<Left>", { noremap = true })
+vim.api.nvim_set_keymap("i", "[", "[]<Left>", { noremap = true })
+vim.api.nvim_set_keymap("i", '"', '""<Left>', { noremap = true })
+vim.api.nvim_set_keymap("i", "'", "''<Left>", { noremap = true })
+vim.api.nvim_set_keymap("i", "`", "``<Left>", { noremap = true })
+vim.api.nvim_set_keymap("i", "<", "<><Left>", { noremap = true })
+vim.api.nvim_set_keymap("i", "/*", "/**/<Left><Left>", { noremap = true })
+
+local lspconfig = require("lspconfig")
+
+-- Shell
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "sh", "bash", "dash" },
+	callback = function()
+		vim.lsp.start({
+			name = "bash-language-server",
+			cmd = { "bash-language-server", "start" },
+		})
+	end,
+})
+
+-- Python
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "py", "ipynb" },
+	callback = function()
+		vim.bo.shiftwidth = 4
+		vim.opt.colorcolumn = "88"
+	end,
+	lspconfig.ruff.setup({}),
+})
+
+-- Go
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "go", "go.mod" },
+	callback = function()
+		vim.bo.expandtab = false
+	end,
+	lspconfig.gopls.setup({}),
+})
+
+-- C
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "c", "cpp", "c++", "h", "hpp", "h++" },
+	callback = function()
+		vim.bo.expandtab = false
+		vim.opt.colorcolumn = "80"
+	end,
+	lspconfig.clangd.setup({}),
+})
